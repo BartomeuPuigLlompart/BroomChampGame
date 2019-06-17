cc395582e42b2e3c567ee9242a369260b2be6db7
Bartomeu Puig <bartomeu.puig@enti.cat>
2019-06-17 04:12:22 +0200

    Primer modo terminado

---
+++ Assets/endingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class endingScript : MonoBehaviour {
+
+    GameObject message;
+    GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.Find("Canvas");
+        message = canvas.transform.GetChild(4).gameObject;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            message.SetActive(true);
+            message.transform.GetChild(1).GetComponent<Text>().text = "You finished " + canvas.transform.GetChild(2).gameObject.GetComponent<Text>().text;
+            StartCoroutine(backToMenu(3.0f));
+        }
+    }
+
+    IEnumerator backToMenu(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene("Principal Menu");
+    }
+}
